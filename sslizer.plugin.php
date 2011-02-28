477cc98adcb856ea2d3c7b77c8a6b1f14d638c8c
Chris Meller <chris@doesnthaveone.com>
2011-02-27 21:05:05 -0500

    Adding files.

---
+++ sslizer.plugin.php
@@ -0,0 +1,21 @@
+<?php
+
+	class SSLizer extends Plugin {
+		
+		public function filter_final_output ( $content ) {
+			
+			if ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] == 'on' ) {
+				
+				$host = Site::get_url('hostname');
+				
+				$content = MultiByte::str_replace( 'http://' . $host, 'https://' . $host, $content );
+				
+			}
+			
+			return $content;
+			
+		}
+		
+	}
+
+?>
